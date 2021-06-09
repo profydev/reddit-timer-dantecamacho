46b4f6e163dd771595ec7412403b69cd280c59b9
Dante Camacho <dantecamachoglz@gmail.com>
2021-06-08 23:18:02 -0500

    folder structure created

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './Routes/index/App';
+import 'normalize.css';
+import './static/index.css';
 
 // eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<App />, document.getElementById('root'));
