5d906102a93c7fd107f415e151834ccbd1a3263c
Dante Camacho <dantecamachoglz@gmail.com>
2021-06-10 17:03:23 -0500

    Folder structure corrections

--- src/Routes/index/App.js
+++ src/Routes/index/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       App Placeholder
+      <Link to="/about">
+        About
+      </Link>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './Routes/index/App';
 import 'normalize.css';
 import './static/index.css';
 
 // eslint-disable-next-line react/jsx-filename-extension
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+
+  document.getElementById('root'),
+);
