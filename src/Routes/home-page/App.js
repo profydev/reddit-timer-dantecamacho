f8783ccfaa799c747bcac06bda3034838e160c47
Dante Camacho <dantecamachoglz@gmail.com>
2021-06-10 23:24:30 -0500

    Link creation progress

---
+++ src/Routes/home-page/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Wrapper from '../../Components/styles/styles';
+
+function App() {
+  return (
+    <Wrapper>
+      <div>
+        App Placeholder
+      </div>
+      <Link to="/search">
+        Search
+      </Link>
+      <Link to="/how-it-works">
+        How it works
+      </Link>
+      <Link to="/about">
+        About
+      </Link>
+
+    </Wrapper>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import App from './Routes/index/App';
+import App from './Routes/home-page/App';
 import 'normalize.css';
 import './static/index.css';
 
